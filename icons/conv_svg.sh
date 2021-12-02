e4ec3ffa8f72b1e6b7b7a393ecce16a37a007c30
freysn <a>
2021-12-02 12:51:03 +0100

    initial commit all files

---
+++ icons/conv_svg.sh
@@ -0,0 +1,5 @@
+pdfseparate icons.pdf icons_%d.pdf
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-filename=zoom_inc.svg icons_3.pdf
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-filename=zoom_dec.svg icons_4.pdf
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-filename=tau_dec.svg icons_2.pdf
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-filename=tau_inc.svg icons_1.pdf
---
+++ icons/cp_png.sh
@@ -0,0 +1,5 @@
+mv icons/icons.001.png tau_inc.png
+mv icons/icons.002.png tau_dec.png
+mv icons/icons.003.png zoom_inc.png
+mv icons/icons.004.png zoom_dec.png
+rm -r icons
